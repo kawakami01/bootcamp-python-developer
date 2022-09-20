21af98d07cad3115deaa230571be664974cc5fcf
Miguel <migskawakami@gmail.com>
2022-09-20 11:21:45 -0300

    Inclusão dos três primeiros desafios de códigos

---
+++ Desafios de códigos/desafio01_Tuitando.py
@@ -0,0 +1,6 @@
+T = input()
+
+if (len(T) > 140):
+  print("MUTE")
+else:
+  print("TWEET")
---
+++ Desafios de códigos/desafio03_Encaixa_ou_nao.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+while(n > 0):
+    n -= 1
+
+    a, b = input().split()
+    
+    if(len(a) < len(b)):
+        print('nao encaixa')
+    else:
+        if(a[-len(b):] == b):
+            print('encaixa')
+        else:
+            print('nao encaixa')
