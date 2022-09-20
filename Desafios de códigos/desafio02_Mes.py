645519e06e92f6a3ba42e9f7b1cd7bd3f71a5a5b
Miguel <migskawakami@gmail.com>
2022-09-20 13:01:15 -0300

    "Adição dos enunciados nos desafios de códigos"

--- Desafios de códigos/desafio02_Mes.py
+++ Desafios de códigos/desafio02_Mes.py
@@ -1,3 +1,14 @@
+'''
+DESAFIO:
+Leia um valor inteiro entre 1 e 12, inclusive. Correspondente a este valor, deve ser apresentado como resposta o mês do ano por extenso, em inglês, com a primeira letra maiúscula.
+
+ENTRADA:
+A entrada contém um único valor inteiro.
+
+SAÍDA:
+Imprima por extenso o nome do mês correspondente ao número existente na entrada, com a primeira letra em maiúscula.
+'''
+
 month = int(input())
 
 months_dict = {
--- Desafios de códigos/desafio03_Encaixa_ou_nao.py
+++ Desafios de códigos/desafio03_Encaixa_ou_nao.py
@@ -1,3 +1,19 @@
+'''
+DESAFIO:
+Paulinho tem em suas mãos um novo problema. Agora a sua professora lhe pediu que construísse um programa para verificar, à partir de dois valores muito grandes A e B, se B corresponde aos últimos dígitos de A.
+
+ENTRADA:
+A entrada consiste de vários casos de teste. A primeira linha de entrada contém um inteiro N que indica a quantidade de casos de teste. Cada caso de teste consiste de dois valores A e B maiores que zero, cada um deles podendo ter até 1000 dígitos.
+
+SAÍDA:
+Para cada caso de entrada imprima uma mensagem indicando se o segundo valor encaixa no primeiro valor, confome exemplo abaixo.
+
+> ENTRADA                               > SAÍDA
+2
+56234523485723854755454545478690 78690   encaixa
+5434554 543                              nao encaixa
+'''
+
 n = int(input())
 
 while(n > 0):
